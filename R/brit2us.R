8eadb2f17203b05215fb6310127d1971c8be1054
Alexander Christensen <alexpaulchristensen@gmail.com>
2020-11-23 12:48:11 -0500

    British-US English conversion

---
+++ R/brit2us.R
@@ -0,0 +1,19 @@
+#British-US English Conversions----
+#' British-US English Converions
+#'
+#' A database to convert between British and US spellings (\emph{n} = 778)
+#' 
+#' @name brit2us
+#' 
+#' @docType data
+#' 
+#' @usage data(brit2us)
+#' 
+#' @format brit2us (list, length = 778)
+#' 
+#' @keywords datasets
+#' 
+#' @examples 
+#' data("brit2us")
+NULL
+#----
