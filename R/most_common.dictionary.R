837b2f88dbf122f17abf463c995f6919bf8b074e
Alexander Christensen <alexpaulchristensen@gmail.com>
2021-12-13 17:25:03 -0500

    added Google's 10k most common words

---
+++ R/most_common.dictionary.R
@@ -0,0 +1,23 @@
+#General Dictionary----
+#' General Dictionary
+#'
+#' A general dictionary of 10,000 of the most common U.S. English words derived from
+#' \href{https://github.com/first20hours/google-10000-english}{https://github.com/first20hours/google-10000-english}.
+#' 
+#' To add additional words to the dictionary, please make an
+#' appendix dictionary (\code{\link[SemNetDictionaries]{append.dictionary}})
+#' 
+#' @name most_common.dictionary
+#' 
+#' @docType data
+#' 
+#' @usage data(most_common.dictionary)
+#' 
+#' @format most_common.dictionary (vector, length = 10000)
+#' 
+#' @keywords datasets
+#' 
+#' @examples 
+#' data("most_common.dictionary")
+NULL
+#----
