57677fefbaa13bb87e34aab336f6410ae1b9dc47
Alexander Christensen <alexpaulchristensen@gmail.com>
2021-12-13 19:11:53 -0500

    added stop words

--- R/most_common.dictionary.R
+++ R/most_common.dictionary.R
@@ -1,5 +1,5 @@
-#General Dictionary----
-#' General Dictionary
+#Most Common Dictionary----
+#' Most Common Dictionary
 #'
 #' A general dictionary of 10,000 of the most common U.S. English words derived from
 #' \href{https://github.com/first20hours/google-10000-english}{https://github.com/first20hours/google-10000-english}.
@@ -13,7 +13,7 @@
 #' 
 #' @usage data(most_common.dictionary)
 #' 
-#' @format most_common.dictionary (vector, length = 10000)
+#' @format most_common.dictionary (vector, length = 9329)
 #' 
 #' @keywords datasets
 #' 
---
+++ R/stop_words.dictionary.R
@@ -0,0 +1,22 @@
+#Stop Words Dictionary---
+#' Stop Words Dictionary
+#'
+#' A selection of stop words that can be removed from semantic responses (\emph{n} = 56)
+#' 
+#' To add additional animals to the dictionary, please make an
+#' appendix dictionary (\code{\link[SemNetDictionaries]{append.dictionary}})
+#' 
+#' @name stop_words.dictionary
+#' 
+#' @docType data
+#' 
+#' @usage data(stop_words.dictionary)
+#' 
+#' @format stop_words.dictionary (vector, length = 56)
+#' 
+#' @keywords datasets
+#' 
+#' @examples 
+#' data("stop_words.dictionary")
+NULL
+#----
