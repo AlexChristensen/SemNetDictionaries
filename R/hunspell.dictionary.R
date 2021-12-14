c05ed7d67951ce18aca2653b36a3494a6a415bee
Alexander Christensen <alexpaulchristensen@gmail.com>
2021-12-13 20:48:44 -0500

    added hunspell dictionary

---
+++ R/hunspell.dictionary.R
@@ -0,0 +1,23 @@
+#hunspell Dictionary----
+#' hunspell Dictionary
+#'
+#' A general dictionary of over 38,000 words from the hunspell dictionary derived from
+#' \href{http://wordlist.aspell.net/dicts/}{http://wordlist.aspell.net/dicts/}.
+#' 
+#' To add additional words to the dictionary, please make an
+#' appendix dictionary (\code{\link[SemNetDictionaries]{append.dictionary}})
+#' 
+#' @name hunspell.dictionary
+#' 
+#' @docType data
+#' 
+#' @usage data(hunspell.dictionary)
+#' 
+#' @format hunspell.dictionary (vector, length = 38530)
+#' 
+#' @keywords datasets
+#' 
+#' @examples 
+#' data("hunspell.dictionary")
+NULL
+#----
