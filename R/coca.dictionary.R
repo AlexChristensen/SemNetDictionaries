6fb0416d95ccf7bcde6c383a0082386d5b4d9be4
Alexander Christensen <alexpaulchristensen@gmail.com>
2021-12-16 10:12:15 -0500

    COCA dictionary and monikers
    o includes over 80,000 words from the Corpus of Contemporary American English
    
    o includes word forms for over 20,000 words from the Corpus of Contemporary American English

---
+++ R/coca.dictionary.R
@@ -0,0 +1,23 @@
+#COCA Dictionary----
+#' Corpus of Contemporary American English Dictionary
+#'
+#' A general dictionary of over 80,000 words from the Corpus of Contemporary American English derived from
+#' \href{https://www.wordfrequency.info/samples.asp}{https://www.wordfrequency.info/samples.asp}.
+#' 
+#' To add additional words to the dictionary, please make an
+#' appendix dictionary (\code{\link[SemNetDictionaries]{append.dictionary}})
+#' 
+#' @name coca.dictionary
+#' 
+#' @docType data
+#' 
+#' @usage data(coca.dictionary)
+#' 
+#' @format coca.dictionary (vector, length = 80409)
+#' 
+#' @keywords datasets
+#' 
+#' @examples 
+#' data("coca.dictionary")
+NULL
+#----
---
+++ R/coca.moniker.R
@@ -0,0 +1,22 @@
+#COCA Moniker----
+#' Corpus of Contemporary American English Moniker
+#'
+#' A database of word forms for the Corpus of Contemporary American English dictionary
+#' 
+#' To add additional COCA monikers to the database, please submit a pull
+#' request or issue to \href{https://github.com/AlexChristensen/SemNetDictionaries}{https://github.com/AlexChristensen/SemNetDictionaries}
+#' 
+#' @name coca.moniker
+#' 
+#' @docType data
+#' 
+#' @usage data(coca.moniker)
+#' 
+#' @format coca.moniker (list, length = 20267)
+#' 
+#' @keywords datasets
+#' 
+#' @examples 
+#' data("coca.moniker")
+NULL
+#----
