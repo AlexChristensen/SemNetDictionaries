de7d10b003e7a94027a5bcf1353c6d9af116be4d
Alexander Christensen <alexpaulchristensen@gmail.com>
2021-12-19 11:11:12 -0500

    combined `coca.dictionary` and `hunspell.dictionary`

---
+++ R/cocaspell.dictionary.R
@@ -0,0 +1,23 @@
+#COCA and Hunspell Dictionary----
+#' Corpus of Contemporary American English and Hunspell Combined Dictionary
+#'
+#' A general dictionary of over 109,000 words from the Corpus of Contemporary American English dictionary
+#' (\code{\link[SemNetDictionaries]{coca.dictionary}}) and Hunspell dictionary (\code{\link[SemNetDictionaries]{hunspell.dictionary}}).
+#' 
+#' To add additional words to the dictionary, please make an
+#' appendix dictionary (\code{\link[SemNetDictionaries]{append.dictionary}})
+#' 
+#' @name cocaspell.dictionary
+#' 
+#' @docType data
+#' 
+#' @usage data(cocaspell.dictionary)
+#' 
+#' @format cocaspell.dictionary (vector, length = 109379)
+#' 
+#' @keywords datasets
+#' 
+#' @examples 
+#' data("cocaspell.dictionary")
+NULL
+#----
---
+++ R/cocaspell.moniker.R
@@ -0,0 +1,22 @@
+#COCA Moniker----
+#' Corpus of Contemporary American English Moniker
+#'
+#' A database of word forms for the Corpus of Contemporary American English dictionary
+#' 
+#' To add additional COCA monikers to the database, please submit a pull
+#' request or issue to \href{https://github.com/AlexChristensen/SemNetDictionaries}{https://github.com/AlexChristensen/SemNetDictionaries}
+#' 
+#' @name cocaspell.moniker
+#' 
+#' @docType data
+#' 
+#' @usage data(cocaspell.moniker)
+#' 
+#' @format cocaspell.moniker (list, length = 20267)
+#' 
+#' @keywords datasets
+#' 
+#' @examples 
+#' data("cocaspell.moniker")
+NULL
+#----
